d56e485310f36f47a073c8f71bb614cb2a0d133a
Olev savi <olev.savi@voco.ee>
2024-09-06 02:08:52 -0400

    kasutaja tervitus

---
+++ praks2/yl1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# See skript tervitab kasutajat tema kasutajanimega
+
+# Väljasta "Tere, " ilma reavahetuseta
+echo -n "Tere, "
+
+# Tekita kasutaja nimi ja väljasta ta ilma reavahetuseta
+echo -n "$(whoami)"
+
+# Väljasta "!" reavahetusega
+echo "!"
