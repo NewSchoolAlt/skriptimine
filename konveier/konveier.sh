9f12d30bca9aff0bda383f0ac4ca28a404ae0788
Olev savi <olev.savi@voco.ee>
2024-09-25 13:48:51 +0300

    e

--- konveier/konveier.sh
+++ konveier/konveier.sh
@@ -1,23 +1,24 @@
+
 #!/bin/bash
 
-# 
+#
 input_file="inimesed.txt"
 output_dir="./"
-first_names_file="$output_dir/eesnimed.txt"
-last_names_file="$output_dir/perenimed.txt"
-domains_file="$output_dir/domeenid.txt"
-usernames_file="$output_dir/kasutajad.txt"
-emails_file="$output_dir/meilid.txt"
+esi_nimed_file="$output_dir/eesnimed.txt"
+perekonna_nimed_file="$output_dir/perenimed.txt"
+domeenid_file="$output_dir/domeenid.txt"
+kasutajad_file="$output_dir/kasutajad.txt"
+meilid_file="$output_dir/meilid.txt"
 
 mkdir -p "$output_dir"
 
-cut -f2 -d"," "$input_file" | cut -f1 -d";" | tr '[:upper:]' '[:lower:]' > "$first_names_file"
+cut -f2 -d"," "$input_file" | cut -f1 -d";" | tr '[:upper:]' '[:lower:]' > "$esi_nimed_file"
 
-cut -f1 -d":" "$input_file" | tr '[:upper:]' '[:lower:]' > "$last_names_file"
+cut -f1 -d":" "$input_file" | tr '[:upper:]' '[:lower:]' | sed 's/[^a-zA-Z]//g' > "$perekonna_nimed_file" 
 
-cut -f2 -d"-" "$input_file" > "$domains_file"
+cut -f2 -d"-" "$input_file" > "$domeenid_file"
 
-paste -d"." "$first_names_file" "$last_names_file" > "$usernames_file"
+paste -d"." "$esi_nimed_file" "$perekonna_nimed_file" > "$kasutajad_file"
 
-paste -d"@" "$usernames_file" "$domains_file" > "$emails_file"
+paste -d"@" "$kasutajad_file" "$domeenid_file" > "$meilid_file"
 
