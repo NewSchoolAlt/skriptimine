81d7a7be5ae5b6b2236a5615fb3244ae4cda128a
Olev savi <olev.savi@voco.ee>
2024-10-02 11:31:55 +0300

    fancy type stuff

--- konveier/konveier.sh
+++ konveier/konveier.sh
@@ -19,16 +19,17 @@ usernames_file="$output_dir/kasutajad.txt"
 #määrame meilide jaoks faili
 emails_file="$output_dir/meilid.txt"
 
-
-#Kontrollib, kas fail on antud
 if [ $# -ne 1 ]; then
-    echo "Kasutamine: $0 <inimesed_fail>"
-    echo "haha noob selline"
-    exit 1
+    echo "Vali inimesed_fail fzf abil (kasuta nooleklahve ja Enterit):"
+    terminal_width=$(tput cols)  # Uus rida, et saada terminali laius
+    input_file=$(find . -type f | fzf --header="Vali fail ja vajuta Enter" --preview="cat {} | toilet --gay --width=$terminal_width")  # Uus rida, et kuvada värviline ASCII kunst
+    if [ -z "$input_file" ]; then
+        echo "Faili ei valitud, väljun."
+        exit 1
+    fi
+else
+    input_file="$1"
 fi
-
-#Määrab input faili
-input_file="$1"
 
 #Teeme output katalooge
 mkdir -p "$output_dir"
@@ -45,9 +46,230 @@ cut -d'-' -f2 "$input_file" | sed 's/^[ \t]*//;s/[ \t]*$//' > "$domains_file"
 paste -d"." "$first_names_file" "$last_names_file" > "$usernames_file"
 paste -d"@" "$usernames_file" "$domains_file" > "$emails_file"
 
+
+
+# Lisame spinneri ootamise animatsiooni
+echo -"Ootan, palun oota"
+echo " "
+spin='/-\|'
+i=0
+while [ "$i" -lt 10 ]; do  # Muuda 10 soovitud ooteaja järgi
+    printf "\r%s" "${spin:i++%${#spin}:1}"  # Kuvab spinneri
+    sleep 0.2  # Ooteaeg
+done
+echo -e "\r"  # Tühjendab spinneri rea
+
 #Kuvame kasutajale loodud failide nimed
 echo "Eesnimed: $first_names_file"
 echo "Perekonnanimed: $last_names_file"
 echo "Domeenid: $domains_file"
 echo "Kasutajatunnused: $usernames_file"
 echo "E-mailid: $emails_file"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo "master hackerman type shit tuleb kohe"
+# Lisame spinneri ootamise animatsiooni
+echo  "Ootan, palun oota"
+spin='/-\|'
+i=0
+while [ "$i" -lt 10 ]; do  # Muuda 10 soovitud ooteaja järgi
+    printf "\r%s" "${spin:i++%${#spin}:1}"  # Kuvab spinneri
+    sleep 0.2  # Ooteaeg
+done
+echo -e "\r"  # Tühjendab spinneri rea
+#Fnacy stuff mis mõtlesin, et panen juurde kuna olen masterhackerman yes yes
+echo " "
+echo " "
+echo "Töötlemine lõpetatud!"
+echo "===================="
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+figlet "made by OLEV!" 
