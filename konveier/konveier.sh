0c84a2f4ccab727f1c91c828f8b42d7e61a87fb1
Olev savi <olev.savi@voco.ee>
2024-10-02 11:07:03 +0300

    remap

--- konveier/konveier.sh
+++ konveier/konveier.sh
@@ -1,41 +1,51 @@
 #!/bin/bash
+echo " "
 
-# Kontrolli, kas failinimi on antud
+#Määrame output kataloogi
+output_dir="./konveier"
+
+#Määrame eesnimede faili
+first_names_file="$output_dir/eesnimed.txt"
+
+#Määrame eesnimede faili
+last_names_file="$output_dir/perenimed.txt"
+
+#Määrame Domeeni faili
+domains_file="$output_dir/domeenid.txt"
+
+#määrame kasutajanimede faili
+usernames_file="$output_dir/kasutajad.txt"
+
+#määrame meilide jaoks faili
+emails_file="$output_dir/meilid.txt"
+
+
+#Kontrollib, kas fail on antud
 if [ $# -ne 1 ]; then
     echo "Kasutamine: $0 <inimesed_fail>"
+    echo "haha noob selline"
     exit 1
 fi
 
-# Failide ja kataloogide määramine
+#Määrab input faili
 input_file="$1"
-output_dir="./konveier"
-first_names_file="$output_dir/eesnimed.txt"
-last_names_file="$output_dir/perenimed.txt"
-domains_file="$output_dir/domeenid.txt"
-usernames_file="$output_dir/kasutajad.txt"
-emails_file="$output_dir/meilid.txt"
 
-# Loome väljundi katalooge
+#Teeme output katalooge
 mkdir -p "$output_dir"
 
-# Eesnimede töötlemine
+#Kasutades sed käsku võtame input faili, lõikane sealt ülejäänud jama ära, jätame alles ainult eesnimed, ning salvestame Määratud faili
 cut -d',' -f2 "$input_file" | sed 's/;.*//;s/[0-9]*-.*//;s/^[ \t]*//;s/[ \t]*$//' > "$first_names_file"
 
-# Perekonnanimede töötlemine
+#Sama loogikaga on ka perenimede fail ja domeenide fail koostatud, ei viitsi pikki kommentaare panna
 cut -d',' -f1 "$input_file" | tr '[:upper:]' '[:lower:]' | sed 's/^[ \t]*//;s/[ \t]*$//' > "$last_names_file"
-
-# Domeenide töötlemine
 cut -d'-' -f2 "$input_file" | sed 's/^[ \t]*//;s/[ \t]*$//' > "$domains_file"
 
-# Muuda kõik tähed väikesteks
-tr '[:upper:]' '[:lower:]' < "$first_names_file" > "$first_names_file.tmp" && mv "$first_names_file.tmp" "$first_names_file"
-tr '[:upper:]' '[:lower:]' < "$last_names_file" > "$last_names_file.tmp" && mv "$last_names_file.tmp" "$last_names_file"
 
-# Loo kasutajanimed ja e-mailid
+#Loob kasutajanimed ja e-mailid failid
 paste -d"." "$first_names_file" "$last_names_file" > "$usernames_file"
 paste -d"@" "$usernames_file" "$domains_file" > "$emails_file"
 
-# Kuvame loodud failide nimed
+#Kuvame kasutajale loodud failide nimed
 echo "Eesnimed: $first_names_file"
 echo "Perekonnanimed: $last_names_file"
 echo "Domeenid: $domains_file"
